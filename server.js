f57a00e02d816c6644cc57ddfdcbdc9401fc1aba
Mauv-hub <mauvpark@gmail.com>
2021-09-05 16:24:54 +0900

    Study(2021.09.05)

---
+++ server.js
@@ -0,0 +1,167 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const {
+	GraphQLSchema,
+	GraphQLObjectType,
+	GraphQLString,
+	GraphQLList,
+	GraphQLInt,
+	GraphQLNonNull,
+} = require("graphql");
+const app = express();
+
+const authors = [
+	{ id: 1, name: "J. K. Rowling" },
+	{ id: 2, name: "J. R. R. Tolkien" },
+	{ id: 3, name: "Brent Weeks" },
+];
+
+const books = [
+	{ id: 1, name: "Harry Potter and the Chamber of Secrets", authorId: 1 },
+	{ id: 2, name: "Harry Potter and the Prisoner of Azkaban", authorId: 1 },
+	{ id: 3, name: "Harry Potter and the Goblet of Fire", authorId: 1 },
+	{ id: 4, name: "The Fellowship of the Ring", authorId: 2 },
+	{ id: 5, name: "The Two Towers", authorId: 2 },
+	{ id: 6, name: "The Return of the King", authorId: 2 },
+	{ id: 7, name: "The Way of Shadows", authorId: 3 },
+	{ id: 8, name: "Beyond the Shadows", authorId: 3 },
+];
+
+// ! Should use funtion to resolve.
+// ! If not, because AuthorType and BookType should be defined before use,
+// ! so it causes infinite error loop.
+
+const BookType = new GraphQLObjectType({
+	name: "Book",
+	description: "Thiss represents a book written by an author",
+	fields: () => ({
+		id: { type: GraphQLNonNull(GraphQLInt) },
+		name: { type: GraphQLNonNull(GraphQLString) },
+		authorId: { type: GraphQLNonNull(GraphQLInt) },
+		author: {
+			type: AuthorType,
+			resolve: (book) => {
+				return authors.find((author) => author.id === book.authorId);
+			},
+		},
+	}),
+});
+
+const AuthorType = new GraphQLObjectType({
+	name: "Author",
+	description: "Thiss represents an author of a book",
+	fields: () => ({
+		id: { type: GraphQLNonNull(GraphQLInt) },
+		name: { type: GraphQLNonNull(GraphQLString) },
+		books: {
+			type: new GraphQLList(BookType),
+			resolve: (author) => {
+				return books.filter((book) => book.authorId === author.id);
+			},
+		},
+	}),
+});
+
+const RootQueryType = new GraphQLObjectType({
+	name: "Query",
+	description: "Root Query",
+	fields: () => ({
+		book: {
+			type: BookType,
+			description: "A Single Books",
+			args: {
+				id: { type: GraphQLInt },
+			},
+			resolve: (parent, args) =>
+				books.find((book) => book.id === args.id),
+		},
+		books: {
+			type: new GraphQLList(BookType),
+			description: "List of All Books",
+			resolve: () => books,
+		},
+		authors: {
+			type: new GraphQLList(AuthorType),
+			description: "List of All Authors",
+			resolve: () => authors,
+		},
+		author: {
+			type: AuthorType,
+			description: "A Single Author",
+			args: {
+				id: { type: GraphQLInt },
+			},
+			resolve: (parent, args) =>
+				authors.find((author) => author.id === args.id),
+		},
+	}),
+});
+
+// ! Should call new GraphQLObjectType,
+// ! Because each object type has different initial values and different params.
+
+const RootMutationType = new GraphQLObjectType({
+	name: "Mutation",
+	description: "Root Mutation",
+	fields: () => ({
+		addBook: {
+			type: BookType,
+			description: "Add a Book",
+			args: {
+				name: { type: GraphQLNonNull(GraphQLString) },
+				authorId: { type: GraphQLNonNull(GraphQLInt) },
+			},
+			resolve: (parent, args) => {
+				const book = {
+					id: books.length + 1,
+					name: args.name,
+					authorId: args.authorId,
+				};
+				books.push(book);
+				return book;
+			},
+		},
+		addAuthor: {
+			type: AuthorType,
+			description: "Add an author",
+			args: {
+				name: { type: GraphQLNonNull(GraphQLString) },
+			},
+			resolve: (parent, args) => {
+				const author = {
+					id: authors.length + 1,
+					name: args.name,
+				};
+				authors.push(author);
+				return author;
+			},
+		},
+		deleteAuthor: {
+			type: AuthorType,
+			description: "Delete an author",
+			args: {
+				name: { type: GraphQLNonNull(GraphQLString) },
+			},
+			resolve: (parent, args) => {
+				const authorIndex = authors.findIndex(
+					(author) => author.name === args.name
+				);
+				authors.splice(authorIndex, 1);
+			},
+		},
+	}),
+});
+
+const schema = new GraphQLSchema({
+	query: RootQueryType,
+	mutation: RootMutationType,
+});
+
+app.use(
+	"/graphql",
+	graphqlHTTP({
+		schema: schema,
+		graphiql: true,
+	})
+);
+app.listen(5000, () => console.log("Server running."));
